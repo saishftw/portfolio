531b998f369f0032753b61ab71f9b5ae2b4fe733
saishftw <ksaish1998@gmail.com>
2024-12-31 01:41:02 +0530

    * bugfix: email button ui

--- components/contact.tsx
+++ components/contact.tsx
@@ -9,8 +9,8 @@ export function Contact() {
   return (
     <section className="py-16 text-center">
       <h2 className={`text-3xl font-bold mb-8  font-mono ${theme === 'dark' ? 'glow-text' : ''}`}>Contact</h2>
-      <Button asChild className="glassmorphism glow-element">
-        <a href="mailto:ksaish1998@gmail.com" className={`${theme == 'dark' ? 'text-white': 'text-black' }`}>
+      <Button variant="outline" asChild className="glassmorphism glow-element">
+        <a href="mailto:ksaish1998@gmail.com">
           <Mail className="mr-2 h-5 w-5" />
           ksaish1998@gmail.com
         </a>
